a313d421dd7de15bdb55165cb4cd0dc89871b4bb
OmarAouini <aouini.omar93@gmail.com>
2021-06-18 00:00:22 +0200

    added recover and json response test

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
@@ -15,13 +16,14 @@ func main() {
 
 	//middlewares
 	app.Use(logger.New(logger.ConfigDefault))
+	app.Use(recover.New())
 
 	//routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hello go fiber!")
 	})
 
-	app.Get("/:name", func(c *fiber.Ctx) error {
+	app.Post("/:name", func(c *fiber.Ctx) error {
 		msg := fmt.Sprintf("hello, %s", c.Params("name"))
 		return c.SendString(msg)
 	})
@@ -29,6 +31,10 @@ func main() {
 	//404 handler
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(404)
+	})
+
+	app.Get("/json", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"success": "true", "message": "messaggio"})
 	})
 
 	//server start
